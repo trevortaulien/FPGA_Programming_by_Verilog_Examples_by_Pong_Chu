fa7fe989ac69fce655bfa7427acf38bbcf072c3f
Trevor Taulien <51167085+trevortaulien@users.noreply.github.com>
2022-09-16 18:57:21 -0400

    initial commit proper naming scheme

---
+++ 2.9.1/2.9.1.srcs/sources_1/new/fourBitGreaterThan.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/16/2022 06:46:52 PM
+// Design Name: 
+// Module Name: fourBitGreaterThan
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fourBitGreaterThan(
+
+    );
+endmodule
