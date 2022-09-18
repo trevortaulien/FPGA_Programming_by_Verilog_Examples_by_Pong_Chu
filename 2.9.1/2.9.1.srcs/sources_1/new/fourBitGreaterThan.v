1fc82d2e5e501345eb1f7ab434a0228b32b71527
Trevor Taulien <51167085+trevortaulien@users.noreply.github.com>
2022-09-18 17:09:56 -0400

    fourBitGreaterThan sim looks good

--- 2.9.1/2.9.1.srcs/sources_1/new/fourBitGreaterThan.v
+++ 2.9.1/2.9.1.srcs/sources_1/new/fourBitGreaterThan.v
@@ -21,6 +21,19 @@
 
 
 module fourBitGreaterThan(
-
+    input [3:0] a, b,
+    output out
     );
+
+    wire eq0_out, eq1_out;
+    wire gt0_out, gt1_out;
+
+    twoBitEqual eq0(.a(a[1:0]), .b(b[1:0]), .eq_out(eq0_out));
+    twoBitEqual eq1(.a(a[3:2]), .b(b[3:2]), .eq_out(eq1_out));
+
+    twoBitGreaterThan gt0(.a(a[1:0]), .b(b[1:0]), .gt_out(gt0_out));
+    twoBitGreaterThan gt1(.a(a[3:2]), .b(b[3:2]), .gt_out(gt1_out));
+
+    assign out = gt1_out | (gt0_out & eq1_out);
+
 endmodule
